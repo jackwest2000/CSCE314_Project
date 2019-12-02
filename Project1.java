70a9254cf93814c536667e8b4feb7babb8ef7d45
jackwest2000 <43962388+jackwest2000@users.noreply.github.com>
2019-12-02 12:54:52 -0600

    Update Project1.java

--- Project1.java
+++ Project1.java
@@ -1,27 +1,16 @@
 // My github repo address is https://github.com/jackwest2000/CSCE314_Project
 
+import java.math.BigInteger;
+
 public class Project1 {
 	public static void main(String[] args) 
 	{
 		// Instantiate Primes Class
-		PrimeOperations testOne = new PrimeOperations();
+		Primes p = new Primes();
 		
-		// Generate Primes and test.
-		testOne.generatePrimes(21);
-		testOne.printPrimes();
-		
-		// Generate and test Twin Primes
-		PrimeOperations testTwo = new PrimeOperations();
-		testTwo.generatePrimes(100);
-		testTwo.generateTwinPrimes();
-		testTwo.printTwins();
-\
-		
-		// Generate and test Hexagonal crosses
-		PrimeOperations testThree = new PrimeOperations();
-		testThree.generatePrimes(2000);
-		testThree.generateTwinPrimes();
-		testThree.generateHexPrimes();
-		testThree.printHexes();
+		// open the main window for the application
+		MainWindow mw = new MainWindow(Config.APPLICATIONNAME, p);
+
 	}
 }
+
